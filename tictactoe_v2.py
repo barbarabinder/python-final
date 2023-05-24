deff554d188a6ce87bbe46a2719b534f146d6144
Your Name <mbp@MBPs-MacBook-Pro.local>
2023-05-24 18:07:18 +0200

    - adding final version of tictactoe_v2.py

--- tictactoe_v2.py
+++ tictactoe_v2.py
@@ -2,9 +2,9 @@
 import random
 from termcolor import colored
 
-print(colored("Tic-Tac-Toe Python Tutorial", "green"))
+print(colored("Tic-Tac-Toe Python Tutorial", "magenta"))
 spiel_aktiv = True
-#spieler_aktuell = 'Stana'
+
 # Spielfeld als Liste erstellen
 spielfeld = [" ",
              colored("1", "black", "on_white"), colored("2","black", "on_white"), colored("3", "black", "on_white"),
@@ -23,23 +23,29 @@ def spielfeld_ausgeben():
 def spieler_eingabe():
     global spiel_aktiv
     while True:
-        spielzug = input("Bitte Feld eingeben: ")
-        # vorzeitiges Spielende durch Spieler
-        if spielzug == 'q':
+        spielzug = input(generate_color_message(spieler_aktuell + ", bitte wähle ein Feld: "))
+
+        if spielzug == 'exit':
+            # Spieler beendet das Spiel
             spiel_aktiv = False
             return
+
+        error_message = generate_color_message("Zahl muss zwischen 1 und 9 liegen")
+
         try:
+            # Input in Nummer umwandeln
             spielzug = int(spielzug)
-        except ValueError:
-            print("Bitte Zahl von 1 bis 9 eingeben")
-        else:
             if spielzug >= 1 and spielzug <= 9:
-                if spielfeld[spielzug] == 'X' or spielfeld[spielzug] == 'O':
+                if spielfeld[spielzug] == spieler1_symbol or spielfeld[spielzug] == spieler2_symbol:
                     print("Das Feld ist bereits belegt - ein anderes wählen!")
                 else:
                     return spielzug
             else:
-                print("Zahl muss zwischen 1 und 9 liegen")
+                raise Exception()
+
+        except Exception:
+            print(error_message)
+
 
 
 def spieler_wechseln(spieler1, spieler2):
@@ -75,43 +81,57 @@ def kontrolle_gewonnen():
 
 
 def kontrolle_auf_unentschieden():
-    if (spielfeld[1] == 'X' or spielfeld[1] == 'O') \
-            and (spielfeld[2] == 'X' or spielfeld[2] == 'O') \
-            and (spielfeld[3] == 'X' or spielfeld[3] == 'O') \
-            and (spielfeld[4] == 'X' or spielfeld[4] == 'O') \
-            and (spielfeld[5] == 'X' or spielfeld[5] == 'O') \
-            and (spielfeld[6] == 'X' or spielfeld[6] == 'O') \
-            and (spielfeld[7] == 'X' or spielfeld[7] == 'O') \
-            and (spielfeld[8] == 'X' or spielfeld[8] == 'O') \
-            and (spielfeld[9] == 'X' or spielfeld[9] == 'O'):
+    if (spielfeld[1] == spieler1_symbol or spielfeld[1] == spieler2_symbol) \
+            and (spielfeld[2] == spieler1_symbol or spielfeld[2] == spieler2_symbol) \
+            and (spielfeld[3] == spieler1_symbol or spielfeld[3] == spieler2_symbol) \
+            and (spielfeld[4] == spieler1_symbol or spielfeld[4] == spieler2_symbol) \
+            and (spielfeld[5] == spieler1_symbol or spielfeld[5] == spieler2_symbol) \
+            and (spielfeld[6] == spieler1_symbol or spielfeld[6] == spieler2_symbol) \
+            and (spielfeld[7] == spieler1_symbol or spielfeld[7] == spieler2_symbol) \
+            and (spielfeld[8] == spieler1_symbol or spielfeld[8] == spieler2_symbol) \
+            and (spielfeld[9] == spieler1_symbol or spielfeld[9] == spieler2_symbol):
         return ('unentschieden')
 
 
+spieler1_color = "blue"
+spieler2_color = "green"
 def spielernamen():
     print()
-    spieler1 = input("Bitte Namen eingeben für Spieler 1: ")
-    spieler2 = input("Bitte Namen eingeben für Spieler 2: ")
+    spieler1 = input(colored("Bitte Namen eingeben für SpielerIn 1: ", spieler1_color, "on_black"))
+    spieler2 = input(colored("Bitte Namen eingeben für SpielerIn 2: ", spieler2_color, "on_black"))
 
     return spieler1, spieler2
 
 
-# aktuelles Spielfeld ausgeben
+# Spieler
 spielfeld_ausgeben()
 spieler1, spieler2 = spielernamen()
 spieler_aktuell = random.choice([spieler1, spieler2])
 
+
+def generate_color_message(message):
+    if spieler_aktuell is spieler1:
+        return colored(message, spieler1_color, "on_black")
+    else:
+        return colored(message, spieler2_color, "on_black")
+
+def print_player_message(message):
+    print(generate_color_message(message))
+
+spieler1_symbol = generate_color_message(colored("x", spieler1_color, "on_black"))
+spieler2_symbol = generate_color_message(colored("o", spieler2_color, "on_black"))
+
 while spiel_aktiv:
     # Eingabe des aktiven Spielers
     print()
-    print("Spieler " + spieler_aktuell + " am Zug")
+    print(generate_color_message("Spieler '" + spieler_aktuell + "' am Zug -"))
     spielzug = spieler_eingabe()
     if spielzug:
-        # spielfeld[spielzug] = 'X'
-        if spieler_aktuell is spieler1:
-            spieler_value = colored("x", "red", "on_black")
 
+        if spieler_aktuell is spieler1:
+            spieler_value = spieler1_symbol
         else:
-            spieler_value = colored("0", "white", "on_black")
+            spieler_value = spieler2_symbol
 
         spielfeld[spielzug] = spieler_value
         # aktuelles Spielfeld ausgeben
@@ -119,7 +139,14 @@ while spiel_aktiv:
         # Kontrolle, ob jemand gewonnen hat
         gewonnen = kontrolle_gewonnen()
         if gewonnen:
-            print("Spieler " + gewonnen + " hat gewonnen!")
+            gewinner = None
+            if (gewonnen == spieler1_symbol):
+                gewinner = spieler1
+            else:
+                gewinner = spieler2
+
+            print(colored("Spieler '" + gewinner + "' hat gewonnen!", "magenta"))
+
             spiel_aktiv = False
             break
         # Kontrolle, ob unentschieden
